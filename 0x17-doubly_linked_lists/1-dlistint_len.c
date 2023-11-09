daec3fb84ef808df45e3dcc74432d4518efc324b
myname <2861988ed@gmail.com>
2023-11-09 03:10:36 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * print_dlistint - prints doubly-linked list.
  * @h: pointer to the head.
@@ -5,13 +7,12 @@
  **/
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
+	size_t x = 0;
 
-	/* while we have a pointer to the list */
 	while (h != NULL)
 	{
-		count++;
+		x++;
 		h = h->next;
 	}
-	return (count);
+	return (x);
 }
